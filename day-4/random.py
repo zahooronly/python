391e39a9f9bcaa2d65d3c7bcdcf3794d41e43b99
Mr. Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-04-22 18:36:23 +0500

    day-4 random

---
+++ day-4/random.py
@@ -0,0 +1,9 @@
+import random
+
+beg = 10
+end = 100
+
+# Generates a random integer between (10, 100)
+random_integer = random.random(beg, end)
+
+print(f"A random integer between {beg} and {end} is: {random_integer}")

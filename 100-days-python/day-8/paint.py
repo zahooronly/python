a61491305201dc6aeff3625b040b21c4a3b535a5
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-04-27 15:25:25 +0500

    paint function

--- 100-days-python/day-8/paint.py
+++ 100-days-python/day-8/paint.py
@@ -1,4 +1,6 @@
 h=int(input('Enter Height of wall '))
 w=int(input('Enter Width of wall '))
 coverage=5
-print(f'This wall can consume {(h*w)/coverage} cans of Paint')
+def paintCalculator(h,w,coverage):
+    print(f'This wall can consume {(h*w)/coverage} cans of Paint')
+paintCalculator(h,w,coverage)

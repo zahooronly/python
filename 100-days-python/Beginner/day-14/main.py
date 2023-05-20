5a0b8f87c425782252731117fc290f249708ec42
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-05-20 11:32:08 +0500

    skull added

--- 100-days-python/Beginner/day-14/main.py
+++ 100-days-python/Beginner/day-14/main.py
@@ -1,3 +1,25 @@
 from gameData import data
 import random
 from art import logo, vs
+
+# Display ART
+
+
+#Generate a random account from game data
+
+#format the account data into printable format.
+
+# Ask user for a guess
+
+# Check if user is correct.
+## get followers count of each account
+## Use if statement to check if user is correct.
+
+
+# Give user feedback on their guess.
+
+#score keeping.
+
+#Make he game repeatable
+
+# Clear the screen between rounds

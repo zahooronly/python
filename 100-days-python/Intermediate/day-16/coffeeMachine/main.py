168b0b0ca983393cf1e383c4a191b353d9091110
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-05-25 16:49:34 +0500

    Money Machine Paused

--- 100-days-python/Intermediate/day-16/coffeeMachine/main.py
+++ 100-days-python/Intermediate/day-16/coffeeMachine/main.py
@@ -1,3 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+moneyMachineObj=MoneyMachine()
+CoffeeMakerObj=CoffeeMaker()
+isOn=True
+
+
+CoffeeMakerObj.report()
+moneyMachineObj.report()

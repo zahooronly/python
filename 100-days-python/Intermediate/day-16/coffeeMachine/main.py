2fe7610d92eab6b6bb9497773432df3c9a68c733
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-05-25 17:23:45 +0500

    coffee Machine completed with OOP concept

--- 100-days-python/Intermediate/day-16/coffeeMachine/main.py
+++ 100-days-python/Intermediate/day-16/coffeeMachine/main.py
@@ -4,8 +4,23 @@ from money_machine import MoneyMachine
 
 moneyMachineObj=MoneyMachine()
 CoffeeMakerObj=CoffeeMaker()
+menu=Menu()
 isOn=True
 
 
 CoffeeMakerObj.report()
 moneyMachineObj.report()
+
+while isOn:
+    options=menu.get_items()
+    choice=input(f"What would you like? ({options})")
+    if choice=='off':
+        isOn=False
+    elif choice=='report':
+        CoffeeMakerObj.report()
+        moneyMachineObj.report()
+    else:
+        drink=menu.find_drink(choice)
+        if CoffeeMakerObj.is_resource_sufficient(drink) and moneyMachineObj.make_payment(drink.cost):
+            CoffeeMakerObj.make_coffee(drink)
+    

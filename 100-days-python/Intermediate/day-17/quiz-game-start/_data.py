92cffce5cfd3f08abb0122c6e893d7a4bae8bd5a
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-06-06 12:05:54 +0500

    extra data added

--- 100-days-python/Intermediate/day-17/quiz-game-start/_data.py
+++ 100-days-python/Intermediate/day-17/quiz-game-start/_data.py
@@ -12,3 +12,26 @@ questionData = [
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+question_data=[
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean","difficulty":"easy",
+        "question":"The Indie Game Development Studio Cing, developers of Hotel     Dusk and Last Window, went bankrupt on March 1st, 2010.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Faust is a playable character in &quot;Guilty Gear Xrd Revelator&quot;.","correct_answer":"True","incorrect_answers":["False"]},
+    {
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Big the Cat is a playable character in &quot;Sonic Generations&quot;.","correct_answer":"False","incorrect_answers":["True"]
+    },
+    {
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In &quot;Super Mario 3D World&quot;, the Double Cherry power-up originated from a developer accidentally making two characters controllable.","correct_answer":"True","incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In Team Fortress 2, being disguised as a scout or medic results in a speed boost.","correct_answer":"False","incorrect_answers":["True"]
+        },{
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Rebecca Chambers does not appear in any Resident Evil except for the original Resident Evil and the Gamecube remake.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In &quot;Need for Speed: Porsche Unleashed&quot;, the player can only drive cars manufactured by Porsche.","correct_answer":"True","incorrect_answers":["False"]},{
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"There are 2 player roles in Trouble in Terrorist Town.","correct_answer":"False","incorrect_answers":["True"]},{
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In the Monster Hunter Series, it is possible to capture Elder Dragons.","correct_answer":"False","incorrect_answers":["True"]},{
+        "category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"&quot;Resident Evil 7&quot; is the first first-person Resident Evil game.","correct_answer":"False","incorrect_answers":["True"]}]

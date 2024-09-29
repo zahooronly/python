dd3a4e9ab3cfc04c3368f1f1b8ce5523f6d011f3
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-09-29 12:48:40 +0500

    DSA with Python removed

--- DSA with Python/0 bigO/bigO.py
+++
@@ -1,7 +0,0 @@
-def printNumbers(n):
-    """
-    Whenever you'll this this function. it will take only one argument as parameter and will print numbers from 0 to that number-1
-    """
-    for i in range(n):
-        print(i)
-printNumbers(10)
